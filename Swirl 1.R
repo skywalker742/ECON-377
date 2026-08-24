086734f94db3dd7625f134f823ade05e19fa9abb
skywalker742 <rmcle2705@gmail.com>
2026-08-24 12:15:51 -0400

    Add files via upload

---
+++ Swirl 1.R
@@ -0,0 +1,36 @@
+#Swirl Lesson 1 Gus McLellan
+
+install.packages('swirl')
+
+library(swirl) # Do once everytime you start session
+
+install_course_github('isaaccloh', '377_swirl')
+
+#starting up swirl 
+
+swirl()
+Mac
+#select course
+1
+#select lesson
+1
+
+play
+1
+1
+1
+1
+1
+5+7
+1
+1
+1
+1
+x <-5+7
+
+
+
+
+
+x
+y <- x-3
