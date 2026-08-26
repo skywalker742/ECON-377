5d023389b4d9c87fb6efd908f470a35d589f3781
skywalker742 <rmcle2705@gmail.com>
2026-08-26 15:13:09 -0400

    Add files via upload

---
+++ Swirl 2.R
@@ -0,0 +1,33 @@
+#Swirl Lesson 3 Gus McLellan
+
+
+install.packages('swirl')
+
+library(swirl) # Do once everytime you start session
+
+install_course_github('isaaccloh', '377_swirl')
+
+#starting up swirl 
+
+swirl()
+Macr
+#select course
+1
+1
+3
+1:20
+pi:10
+15:1
+?':'
+seq(1,20)
+seq(0, 10, by=0.5)
+my_seq <- seq(5, 10, length=30)
+length(my_seq)
+1:length(my_seq)
+seq(along.with = my_seq)
+seq_along(my_seq)
+rep(0, times = 40)
+rep(c(0, 1, 2), times = 10)
+rep(c(0,1,2), each = 10)
+2
+0
