908d20882bf2f04fbf7629b58753e679f149c05e
skywalker742 <rmcle2705@gmail.com>
2026-08-26 14:12:27 -0400

    Update Swirl 1.R

--- Swirl 1.R
+++ Swirl 1.R
@@ -15,3 +15,23 @@ Mac
 #select lesson
 1
 5+7
+x <- 5 + 7
+x
+y<- x-3
+y
+z <- c(1.1,9,3.14)
+?c
+z
+c(z,555,z)
+z*2+1000
+my_sqrt <- sqrt(z-1)
+3
+my_sqrt
+my_div <- z / my_sqrt
+3
+my_div
+c(1, 2, 3, 4) + c(0, 10)
+c(1, 2, 3, 4) + c(0, 10, 100)
+my_div
+1
+0
