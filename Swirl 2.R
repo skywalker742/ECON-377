d247b6ab3436ce8da0848ed8e1dc818a58697dbb
skywalker742 <rmcle2705@gmail.com>
2026-09-10 16:32:21 -0400

    Create Swirl 2.R

---
+++ Swirl 2.R
@@ -0,0 +1,23 @@
+#Swirl Lesson 2 Gus McLellan
+
+install.packages('swirl')
+
+library(swirl) # Do once everytime you start session
+
+install_course_github('isaaccloh', '377_swirl')
+
+#starting up swirl 
+
+swirl()
+Macg
+
+
+#select course
+1
+
+#select lesson
+2
+
+getwd()
+ls()
+
