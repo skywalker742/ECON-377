c9358e7db249afbd88eccb8121d9ff5563cf4712
skywalker742 <rmcle2705@gmail.com>
2026-09-10 15:08:17 -0400

    Create Swirl 4.R

---
+++ Swirl 4.R
@@ -0,0 +1,30 @@
+#Swirl Lesson 4 Gus McLellan
+
+install.packages('swirl')
+
+library(swirl) # Do once everytime you start session
+
+install_course_github('isaaccloh', '377_swirl')
+
+#starting up swirl 
+
+swirl()
+Macg
+
+
+#select course
+1
+
+#select lesson
+4
+
+
+
+
+num_vect <- c(0.5, 55.0, -10.0, 6.0)
+
+tf <- num_vect < 1
+
+1 # I think it will look like a vector of 4 logical values
+
+tf
